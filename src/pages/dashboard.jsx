be28171427f89c72658e0b19540c4673e72827b0
shrutisinha74168 <shrutisinha74168@gmail.com>
2026-02-18 13:16:37 +0530

    something chnges

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Dashboard.css"; 
 
 const Dashboard = () => {
   const [profile, setProfile] = useState(null);
